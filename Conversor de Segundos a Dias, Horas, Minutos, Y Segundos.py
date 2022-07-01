1e6749013fc0aefe3fdbb9ea0fd802ea597423dc
ElianY7 <105828833+ElianY7@users.noreply.github.com>
2022-07-01 19:55:49 -0300

    Update Conversor de Segundos a Dias, Horas, Minutos, Y Segundos.py

--- Conversor de Segundos a Dias, Horas, Minutos, Y Segundos.py
+++ Conversor de Segundos a Dias, Horas, Minutos, Y Segundos.py
@@ -3,10 +3,10 @@ sec=int(input("Ingrese segundos a convertir: "))
 dias=sec//86400    #Hay 86400 segundos en un día
 sec%=86400
 
-horas=sec//3600
+horas=sec//3600    #Hay 3600 segundos en una hora
 sec%=3600
 
-minutos=sec//60
+minutos=sec//60    #Hay 60 segundos en un minuto
 sec%=60
 
 print("Hay",dias,"dias,",horas,"horas,",minutos,"minutos y",sec,"segundos.")
